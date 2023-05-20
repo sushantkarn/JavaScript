de517f37c7f86af6e260ee9f938bc4c9809f3f53
Sushant Karn <115387037+sushantkarn@users.noreply.github.com>
2023-05-19 21:21:58 -0700

    Add files via upload

---
+++ Javascript--Learning/Chapter 1/03_let_var_const.js
@@ -0,0 +1,12 @@
+console.log("JavaScript Tutorial about let,var and const")
+var a= 45;
+const author="Sushant"
+let b= "Sushant";
+var c = null
+var d= undefined
+{
+  let b = "Sagar"
+  console.log(b)
+}
+console.log(b)
+console.log("Sushant")
